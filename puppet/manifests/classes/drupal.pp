dd5f6dd8c5e1d0fba5d00e95aa7d768774227244
Benjamin Doherty <bjd@bangpound.org>
2013-04-09 19:14:28 -0500

    add imagemagick.

--- puppet/manifests/classes/drupal.pp
+++ puppet/manifests/classes/drupal.pp
@@ -2,6 +2,10 @@ class drupal {
   include workspace_mysql
   include workspace_apache_php
 
+  package { imagemagick:
+    ensure => installed,
+  }
+
   # install drush, we use this method over the ubuntu package as that requires
   # a drush self-update that prompts for a version. This method uses drush's
   # official pear channel.
